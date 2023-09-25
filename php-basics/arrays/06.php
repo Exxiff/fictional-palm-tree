d4eba86e980d6de9c075282ce60e59437ff7c716
exxiff <exxiff@gmail.com>
2023-09-25 20:16:45 +0300

    Refactored code, added functions

--- php-basics/arrays/06.php
+++ php-basics/arrays/06.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 $wordBank = [
     'bicycle', 'diamond', 'lantern',
@@ -6,48 +6,85 @@ $wordBank = [
     'octopus', 'wallet', 'pneumonoultramicroscopicsilicovolcanoconiosis',
 ];
 
+function GameStateMessage($gameState, $playerWordString): string
+{
+    $message = '';
+    switch ($gameState) {
+        case "win":
+            $message = "You win! The word was " . strtoupper($playerWordString) . PHP_EOL;
+            break;
+        case "lose":
+            $message = "You lost, no guesses left. The word was " . strtoupper($playerWordString) . PHP_EOL;
+            break;
+    }
+    return $message;
+}
+
 function sanitizeInput(string $input): string
 {
-    return strtolower(trim($input));
+    return strtolower(str_replace(' ', '', $input));
+}
+
+function validateInput(string $input): bool
+{
+    return strlen($input) === 1 && ctype_alpha($input);
+}
+
+function getGameInfo(int $guessCount, array $gameBoard, string $misses, bool $validInput): string
+{
+    $info = "-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n";
+    $info .= "Attempts left: $guessCount\n\n";
+    $info .= "Word: " . implode(' ', $gameBoard) . "\n\n";
+    $info .= "Misses: " . $misses . "\n\n";
+    if ($validInput === false) {
+        $info .= "Invalid input! Please enter a single letter\n\n";
+    }
+    return $info;
 }
 
 $playAgain = true;
 while ($playAgain === true) {
-    $randomWord = $wordBank[array_rand($wordBank)];
-    $splitWord = str_split($randomWord);
+
     $guessCount = 7;
-    $misses = '';
-    $gameBoard = array_fill(0, strlen($randomWord), '_');
+    $missedLetters = '';
+    $gameStatus = 'continue';
+    $validInput = true;
 
-    while ($guessCount > 0) {
+    $playerWordString = $wordBank[array_rand($wordBank)];
+    $playerLetterArray = str_split($playerWordString);
+    $gameBoardArray = array_fill(0, strlen($playerWordString), '_');
+
+    while ($gameStatus === 'continue') {
+        var_dump($validInput);
         $correctGuess = false;
-        echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n";
-        echo "Attempts left: $guessCount\n\n";
-        echo "Word : " . implode(' ', $gameBoard) . "\n\n";
-        echo "Mises : " . $misses . PHP_EOL . PHP_EOL;
-        $userInput = (string)readline('Guess : ');
-        $userInput = sanitizeInput($userInput);
 
-        for ($i = 0; $i < strlen($randomWord); $i++) {
-            if ($userInput === $splitWord[$i]) {
-                $gameBoard[$i] = $userInput;
+        echo getGameInfo($guessCount, $gameBoardArray, $missedLetters, $validInput);
+        $userInput = sanitizeInput(readline('Guess: '));
+        $validInput = validateInput($userInput);
+        if (validateInput($userInput) === false) {
+            continue; // restarts loop
+        }
+
+        for ($i = 0; $i < strlen($playerWordString); $i++) {
+            if ($userInput === $playerLetterArray[$i]) {
+                $gameBoardArray[$i] = $userInput;
                 $correctGuess = true;
             }
         }
-
         if ($correctGuess === false) {
             $guessCount--;
-            $misses .= $userInput;
+            $missedLetters .= $userInput;
+        }
+        if ($gameBoardArray === $playerLetterArray) {
+            echo getGameInfo($guessCount, $gameBoardArray, $missedLetters, $validInput);
+            $gameStatus = 'win';
+        }
+        if ($guessCount <= 0) {
+            echo getGameInfo($guessCount, $gameBoardArray, $missedLetters, $validInput);
+            $gameStatus = 'lose';
         }
 
-        if ($gameBoard === $splitWord) {
-            echo "You win! The word was " . strtoupper($randomWord) . PHP_EOL;
-            break;
-        }
-
-        if ($guessCount === 0) {
-            echo "You lost, no guesses left. The word was " . strtoupper($randomWord) . PHP_EOL;
-        }
+        echo GameStateMessage($gameStatus, $playerWordString);
     }
     while (true) {
         $playAgainAnswer = sanitizeInput(readline('Play "again" or "quit"? : '));
